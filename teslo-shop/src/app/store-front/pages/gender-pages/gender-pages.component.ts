e3f684d27aba6e334871ff1c05d0ce6450cdc9f4
Gioh <gioh2020@gmail.com>
2025-04-30 18:45:15 -0500

    devGio/create/ se crean los componentes de la seccion pages de storefront

---
+++ teslo-shop/src/app/store-front/pages/gender-pages/gender-pages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gender-pages',
+  imports: [],
+  templateUrl: './gender-pages.component.html',
+  styleUrl: './gender-pages.component.css',
+})
+export class GenderPagesComponent { }
