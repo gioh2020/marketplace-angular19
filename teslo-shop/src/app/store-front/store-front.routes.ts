d1bc84e5383a402c53bc365024be42041d3e4897
Gioh <gioh2020@gmail.com>
2025-04-30 18:56:32 -0500

    devGio/create/ se crean las rutas para storefront

--- teslo-shop/src/app/store-front/store-front.routes.ts
+++ teslo-shop/src/app/store-front/store-front.routes.ts
@@ -1,11 +1,32 @@
 import { Routes } from "@angular/router";
 import { StoreFrontLayoutComponent } from "./layouts/store-front-layout/store-front-layout.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { GenderPagesComponent } from "./pages/gender-pages/gender-pages.component";
+import { ProductPageComponent } from "./pages/product-page/product-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
 
 export const storeFrontRoutes: Routes = [
     {
         path: '',
         component: StoreFrontLayoutComponent,
-        children: []
+        children: [
+            {
+                path: '',
+                component: HomePageComponent,
+            },
+            {
+                path: 'gender/:gender',
+                component: GenderPagesComponent,
+            },
+            {
+                path: 'product/:idSlug',
+                component: ProductPageComponent,
+            },
+            {
+                path: '**',
+                component: NotFoundPageComponent,
+            }
+        ]
     },
     {
         path: '**',
