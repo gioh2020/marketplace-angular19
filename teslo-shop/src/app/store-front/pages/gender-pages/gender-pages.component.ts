2410b62c00ab19f954798415adc51a2d32740841
Gioh <gioh2020@gmail.com>
2025-05-05 06:23:13 -0500

    devGio/ se eliminan los archivos css

--- teslo-shop/src/app/store-front/pages/gender-pages/gender-pages.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-gender-pages',
-  imports: [],
-  templateUrl: './gender-pages.component.html',
-  styleUrl: './gender-pages.component.css',
-})
-export class GenderPagesComponent { }
